2711b6fd71f74879ecde935493ee7e4a086b5717
Asaniyan Ayomide <heritageayomide2017@gmail.com>
2022-04-02 21:59:31 +0100

    fixed fetch api bugs

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,35 @@
-import FetchCardInfo from "./FetchCardInfo";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-    const {data, isPending, error} = FetchCardInfo('../db.json');
+
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const abortCont = new AbortController();
+
+        fetch('https://asaniyan.github.io/jsonapi/db.json', {signal: abortCont.signal })
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setData(data.allGiftCards);
+            setIsPending(false);
+            setError(null);
+            //console.log(data.allGiftCards);
+        }).catch(err => {
+          if (err.name === 'AbortError') {
+              console.log('fetch aborted');
+          } else {
+            setIsPending(false);
+            setError(err.message);
+          }
+      })
+        return () => abortCont.abort();
+
+    }, [])
+
 
 
     return (
